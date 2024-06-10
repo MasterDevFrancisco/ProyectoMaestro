f66500c1c1df6b79c868c6a4ffe88e77d888f38c
MasterDevFrancisco <programador@cmx360.com.mx>
2024-06-10 16:15:58 -0600

    Hay que validar los errores en las excepciones de cuando se manda a llamar a la API de convertio

--- app/Livewire/Catalogos/FormatosComponent.php
+++ app/Livewire/Catalogos/FormatosComponent.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Livewire\Catalogos;
 
 use App\Models\Formatos;
@@ -10,6 +11,7 @@ use Illuminate\Support\Facades\Storage;
 use App\Models\Elementos;
 use Livewire\WithFileUploads;
 use Livewire\Attributes\On;
+use Illuminate\Support\Facades\Log;
 use GuzzleHttp\Client;
 
 #[Title('Formatos')]
@@ -62,18 +64,18 @@ class FormatosComponent extends Component
             'elementos_id' => 'required|exists:elementos,id',
             'documento' => 'required|max:2048'
         ];
-    
+
         $this->validate($rules);
-    
+
         if ($this->documento) {
             // Generar el nombre del archivo basado en el nombre proporcionado por el usuario
             $nombreDoc = 'formatos/' . preg_replace('/[^a-zA-Z0-9-_\.]/', '_', $this->nombre) . '.' . $this->documento->extension();
             $this->documento->storeAs('public', $nombreDoc);
-    
+
             // Guardar la ruta_pdf del documento en la variable ruta_pdf
             $this->ruta_pdf = $nombreDoc;
         }
-    
+
         // Crear un nuevo registro en la tabla Formatos
         $formatosInsert = new Formatos();
         $formatosInsert->nombre = $this->nombre;
@@ -82,26 +84,29 @@ class FormatosComponent extends Component
         $formatosInsert->eliminado = 0;
         $formatosInsert->convertio_id = 666;
         $formatosInsert->ruta_html = "ruta/test";
-    
+
         $formatosInsert->save();
-    
+
         // Llamar a la función convertToHtml después de guardar el registro
-        $conversionResult = $this->convertToHtml($formatosInsert->id);
-    
+        $conversionResult = $this->convertToHtml($formatosInsert->id,$formatosInsert->id);
+        
+
+        Log::info("ConversionResult: " . print_r($conversionResult, true));
+
         // Mostrar una alerta con el resultado de la API
-        dd($conversionResult);
-    
+
+
         // Actualizar el total de filas
         $this->totalRows = Formatos::where('eliminado', 0)->count();
-    
+
         // Cerrar el modal y mostrar un mensaje de éxito
         $this->dispatch('close-modal', 'modalFormato');
         $this->dispatch('msg', 'Registro creado correctamente');
-    
+
         // Resetear los campos del formulario
         $this->reset(['nombre', 'ruta_pdf', 'elementos_id', 'documento']);
     }
-    
+
     public function update()
     {
         $rules = [
@@ -124,8 +129,8 @@ class FormatosComponent extends Component
         $formatosInsert->nombre = $this->nombre;
         $formatosInsert->elementos_id = $this->elementos_id;
         $formatosInsert->eliminado = 0;
-        $formatosInsert->convertio_id = 666;
-        $formatosInsert->ruta_html = "ruta/test";
+        $formatosInsert->convertio_id = 0;
+        $formatosInsert->ruta_html = "";
 
         // Si se ha subido un nuevo documento, se actualiza la ruta_pdf
         if ($this->documento) {
@@ -209,6 +214,88 @@ class FormatosComponent extends Component
         $this->dispatch('msg', 'Registro eliminado correctamente');
     }
 
+    public function convertToHtml($id,$idRegistro)
+    {
+        $client = new Client();
+        $response = $client->post('https://api.convertio.co/convert', [
+            'headers' => [
+                'Content-Type' => 'application/json',
+            ],
+            'json' => [
+                'apikey' => 'cc1e13a4738b02abbce510862464f0a4',
+                'file' => "https://desarrollospatito.com/project/tester/cmx360/uploads/EJEMPLO%20DEL%20ELEMENTO%20I.docx.pdf",
+                'outputformat' => 'html'
+            ]
+        ]);
+
+        $result = json_decode($response->getBody(), true);
+
+        if ($result['code'] == 200 && $result['status'] == 'ok') {
+            $statusResult = $this->getConversionStatus($result['data']['id']);
+            if ($statusResult['code'] === 200 && $statusResult['status'] === 'ok') {
+                // Verificar si 'data', 'output' y 'url' existen
+                if (isset($statusResult['data']['output']['url'])) {
+                    // Procesar el resultado exitoso
+                    Log::info('Retorno: ' . $statusResult['data']['output']['url']);
+                    // dd($statusResult['data']['output']);
+                    $formatoHtml = Formatos::findOrFail($idRegistro);
+                    $formatoHtml->convertio_id = $result['data']['id'];
+                    $formatoHtml->ruta_html = $statusResult['data']['output']['url'];
+                    $formatoHtml->save();
+
+                    return $statusResult;
+                } else {
+                    // Manejar el caso en que 'url' no existe
+                    Log::error('El campo "url" no está definido en la respuesta', $statusResult);
+                    // Puedes retornar algún valor de error o lanzar una excepción dependiendo de tu lógica de negocio
+                    return null;
+                }
+            } else {
+                // Manejar el caso de error en el código de estado o status
+                Log::error('Error en la respuesta de la API', $statusResult);
+                // Puedes retornar algún valor de error o lanzar una excepción dependiendo de tu lógica de negocio
+                return null;
+            }
+        } else {
+            // Mostrar un mensaje de error si la solicitud inicial falla
+
+            //$this->dispatch('msg', 'Error al iniciar la conversión. Inténtelo de nuevo más tarde.');
+            return $result;
+        }
+    }
+
+
+    private function getConversionStatus($conversionId)
+    {
+        sleep(5);
+        Log::info('Durmiendo 5 segundos');
+        $client = new Client();
+        $attempts = 0;
+        $maxAttempts = 3;
+        $result = null;
+
+        while ($attempts < $maxAttempts) {
+            $response = $client->get("https://api.convertio.co/convert/{$conversionId}/status", [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                ]
+            ]);
+
+            $result = json_decode($response->getBody(), true);
+
+            if ($result['code'] === 200 && $result['status'] === 'ok') {
+                return $result;
+            }
+
+            $attempts++;
+            sleep(2); // Esperar 2 segundos antes de intentar de nuevo
+            Log::info('Durmiendo 2 segundos');
+        }
+
+        Log::info('Conversion status', ['result' => $result]); // Registrar el último resultado obtenido
+
+        return $result; // Retornar el último resultado obtenido
+    }
     /* public function convertToHtml($id)
     {
         $formato = Formatos::findOrFail($id);
@@ -234,25 +321,4 @@ class FormatosComponent extends Component
         $result = json_decode($response->getBody(), true);
         return $result;
     } */
-
-    public function convertToHtml($id)
-    {
-  
-    
-        $client = new Client();
-        $response = $client->post('https://api.convertio.co/convert', [
-            'headers' => [
-                'Content-Type' => 'application/json',
-            ],
-            'json' => [
-                'apikey' => 'cc1e13a4738b02abbce510862464f0a4',
-                'file' => "https://desarrollospatito.com/project/tester/cmx360/uploads/EJEMPLO%20DEL%20ELEMENTO%20I.docx.pdf",
-                'outputformat' => 'html'
-            ]
-        ]);
-    
-        $result = json_decode($response->getBody(), true);
-        return $result;
-    }
-    
 }
