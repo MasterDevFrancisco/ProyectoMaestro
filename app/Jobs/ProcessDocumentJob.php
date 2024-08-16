8eab8fc699f1d57037acc119c8b9f1c920bb4cec
MasterDevFrancisco <programador@cmx360.com.mx>
2024-08-16 11:24:39 -0600

    Ya realiza la descarga del archivo zip La descarga se hace correctamente desde el boton de las notificaciones

--- app/Jobs/ProcessDocumentJob.php
+++ app/Jobs/ProcessDocumentJob.php
@@ -35,117 +35,123 @@ class ProcessDocumentJob implements ShouldQueue
     }
 
     public function handle(): void
-{
-    $user = User::find($this->userId);
-    $elemento = $this->loadElemento($this->elementoId);
-    Log::info($elemento);
+    {
+        $user = User::find($this->userId);
+        $elemento = $this->loadElemento($this->elementoId);
+        Log::info($elemento);
 
-    if ($elemento) {
-        // Obtener formatos y tablas relacionados
-        $formatos = Formatos::where('elementos_id', $elemento->id)
-            ->where('eliminado', 0)
-            ->get();
-        $formatosIds = $formatos->pluck('id');
-        $tablas = Tablas::whereIn('formatos_id', $formatosIds)->get();
+        if ($elemento) {
+            // Obtener formatos y tablas relacionados
+            $formatos = Formatos::where('elementos_id', $elemento->id)
+                ->where('eliminado', 0)
+                ->get();
+            $formatosIds = $formatos->pluck('id');
+            $tablas = Tablas::whereIn('formatos_id', $formatosIds)->get();
 
-        $camposTexto = [];
+            $camposTexto = [];
 
-        // Obtener los campos de cada tabla
-        foreach ($tablas as $tabla) {
-            $getCampos = Campos::where('tablas_id', $tabla->id)->get();
-
-            foreach ($getCampos as $campo) {
-                $camposTexto[$tabla->nombre][$campo->linkname] = $campo->nombre_columna;
-            }
-        }
-
-        $missingFields = [];
-        $camposConValores = [];
-
-        // Verificar campos vacíos y preparar datos para la API
-        foreach ($formatos as $formato) {
-            $rutaPdf = $formato->ruta_pdf;
-            $camposConValores[$rutaPdf] = []; // Inicializar el array para este formato específico
-
+            // Obtener los campos de cada tabla
             foreach ($tablas as $tabla) {
-                if ($tabla->formatos_id == $formato->id) { // Verificar si la tabla pertenece al formato actual
-                    foreach ($camposTexto[$tabla->nombre] as $linkname => $nombre) {
-                        if (empty($this->formData[$linkname])) {
-                            $missingFields[] = $nombre;
-                        } else {
-                            $camposConValores[$rutaPdf][$linkname] = $this->formData[$linkname];
+                $getCampos = Campos::where('tablas_id', $tabla->id)->get();
+
+                foreach ($getCampos as $campo) {
+                    $camposTexto[$tabla->nombre][$campo->linkname] = $campo->nombre_columna;
+                }
+            }
+
+            $missingFields = [];
+            $camposConValores = [];
+
+            // Verificar campos vacíos y preparar datos para la API
+            foreach ($formatos as $formato) {
+                $rutaPdf = $formato->ruta_pdf;
+                $camposConValores[$rutaPdf] = []; // Inicializar el array para este formato específico
+
+                foreach ($tablas as $tabla) {
+                    if ($tabla->formatos_id == $formato->id) { // Verificar si la tabla pertenece al formato actual
+                        foreach ($camposTexto[$tabla->nombre] as $linkname => $nombre) {
+                            if (empty($this->formData[$linkname])) {
+                                $missingFields[] = $nombre;
+                            } else {
+                                $camposConValores[$rutaPdf][$linkname] = $this->formData[$linkname];
+                            }
                         }
                     }
                 }
             }
-        }
 
-        // Si hay campos vacíos, registrar error y detener
-        if (!empty($missingFields)) {
-            $missingFieldsStr = implode(",", $missingFields);
-            Log::error("Los siguientes campos no pueden estar vacíos: {$missingFieldsStr}");
-            return;
-        }
-
-        $resultados = [];
-        $client = new Client();
-
-        // Enviar datos a la API y procesar la respuesta
-        foreach ($camposConValores as $rutaPdf => $reemplazos) {
-            $json_data = json_encode([$rutaPdf => $reemplazos]);
-            Log::info("Enviando datos a la API:", ['data' => $json_data]);
-
-            try {
-                $response = $client->post('http://127.0.0.1:5000/replace-text', [
-                    'headers' => ['Content-Type' => 'application/json'],
-                    'body' => $json_data,
-                    'timeout' => 300,
-                    'connect_timeout' => 300,
-                ]);
-
-                $responseBody = json_decode($response->getBody(), true);
-
-                if ($response->getStatusCode() === 200) {
-                    $uniqueId = $responseBody['unique_id'] ?? 'N/A';
-                    $filePath = "C:\\laragon\\www\\ProyectoMaestro\\public\\storage\\public\\pdf\\{$uniqueId}.pdf";
-                    Log::info('Documento procesado exitosamente con la ruta:', ['file_path' => $filePath]);
-                    $resultados[$filePath] = $uniqueId;
-                } else {
-                    Log::error('Hubo un problema al procesar el documento.');
-                }
-            } catch (\Exception $e) {
-                Log::error('Error al conectarse con el servicio Flask: ' . $e->getMessage());
+            // Si hay campos vacíos, registrar error y detener
+            if (!empty($missingFields)) {
+                $missingFieldsStr = implode(",", $missingFields);
+                Log::error("Los siguientes campos no pueden estar vacíos: {$missingFieldsStr}");
+                return;
             }
-        }
 
-        // Crear un archivo ZIP con los PDFs generados usando el nombre del elemento
-        $nombreZip = str_replace(' ', '_', $elemento->nombre) . '.zip';
-        $zipFilePath = "C:\\laragon\\www\\ProyectoMaestro\\public\\storage\\public\\pdf\\{$nombreZip}";
-        $zip = new ZipArchive();
-        if ($zip->open($zipFilePath, ZipArchive::CREATE) === TRUE) {
-            $totalArchivos = count($resultados);
-            $i = 1;
-            foreach ($resultados as $filePath => $uniqueId) {
-                if (file_exists($filePath)) {
-                    Log::info("Archivo {$i} de {$totalArchivos} agregado al ZIP:", ['file_path' => $filePath]);
-                    $zip->addFile($filePath, basename($filePath));
-                    $i++;
+            $resultados = [];
+            $client = new Client();
+
+            // Enviar datos a la API y procesar la respuesta
+            foreach ($camposConValores as $rutaPdf => $reemplazos) {
+                $json_data = json_encode([$rutaPdf => $reemplazos]);
+                Log::info("Enviando datos a la API:", ['data' => $json_data]);
+
+                try {
+                    $response = $client->post('http://127.0.0.1:5000/replace-text', [
+                        'headers' => ['Content-Type' => 'application/json'],
+                        'body' => $json_data,
+                        'timeout' => 300,
+                        'connect_timeout' => 300,
+                    ]);
+
+                    $responseBody = json_decode($response->getBody(), true);
+
+                    if ($response->getStatusCode() === 200) {
+                        $uniqueId = $responseBody['unique_id'] ?? 'N/A';
+                        $filePath = "C:\\laragon\\www\\ProyectoMaestro\\public\\storage\\public\\pdf\\{$uniqueId}.pdf";
+                        Log::info('Documento procesado exitosamente con la ruta:', ['file_path' => $filePath]);
+                        $resultados[$filePath] = $uniqueId;
+                    } else {
+                        Log::error('Hubo un problema al procesar el documento.');
+                    }
+                } catch (\Exception $e) {
+                    Log::error('Error al conectarse con el servicio Flask: ' . $e->getMessage());
                 }
             }
-            $zip->close();
+
+            // Crear un archivo ZIP con los PDFs generados usando el nombre del elemento
+            $nombreZip = str_replace(' ', '_', $elemento->nombre) . '.zip';
+            $zipFilePath = public_path("storage/public/pdf/{$nombreZip}");
+            Log::info($zipFilePath);
+
+            $zip = new ZipArchive();
+            if ($zip->open($zipFilePath, ZipArchive::CREATE) === TRUE) {
+                $totalArchivos = count($resultados);
+                $i = 1;
+                foreach ($resultados as $filePath => $uniqueId) {
+                    if (file_exists($filePath)) {
+                        Log::info("Archivo {$i} de {$totalArchivos} agregado al ZIP:", ['file_path' => $filePath]);
+                        $zip->addFile($filePath, basename($filePath));
+                        $i++;
+                    }
+                }
+                $zip->close();
+            } else {
+                Log::error('No se pudo crear el archivo ZIP.');
+                return;
+            }
+
+            // Convertir la ruta del ZIP en una URL accesible
+            $zipUrl = asset("storage/public/pdf/{$nombreZip}");
+
+            // Enviar la notificación con la URL del ZIP
+            Notification::send($user, new ZipCreatedNotification($zipUrl));
+
+            // Registrar éxito y finalizar
+            Log::info('Archivo ZIP creado exitosamente en la ruta:', ['zip_file_path' => $zipUrl]);
         } else {
-            Log::error('No se pudo crear el archivo ZIP.');
-            return;
+            Log::error("No se encontró el elemento con ID: {$this->elementoId}");
         }
-
-        Notification::send($user, new ZipCreatedNotification($zipFilePath));
-
-        // Registrar éxito y finalizar
-        Log::info('Archivo ZIP creado exitosamente en la ruta:', ['zip_file_path' => $zipFilePath]);
-    } else {
-        Log::error("No se encontró el elemento con ID: {$this->elementoId}");
     }
-}
 
 
     protected function loadElemento($elementoId)
